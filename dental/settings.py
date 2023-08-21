1fcf48ba63f6ef1f26c1291331bd39fe12fbfe20
Vasurko09 <91550054+Vasurko09@users.noreply.github.com>
2023-08-21 14:37:38 +0530

    Update settings.py

--- dental/settings.py
+++ dental/settings.py
@@ -108,9 +108,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-MEDIA_URL = '/images/'
-MEDIA_ROOT =BASE_DIR / 'static/imgs'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
